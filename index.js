36875b3d8699a9f7573818b1d3e440618d6f18ea
mikey247 <michaeljunior794@gmail.com>
2022-11-04 01:10:53 +0100

    post task

--- index.js
+++ index.js
@@ -20,31 +20,40 @@ app.get("/", (req, res) => {
   });
 });
 
+const Enum = Object.freeze({
+  subtraction: "subtraction",
+  addition: "addition",
+  multiplication: "multiplication",
+});
+
 app.post("/", (req, res) => {
+  console.log(req.body);
   const { operation_type, x, y } = req.body;
 
-  if (operation_type.trim() === "subtraction") {
-    res.status(200).json({
-      slackUsername: "mikey",
-      result: x - y,
-      operation_type,
-    });
-  }
+  switch (operation_type.value) {
+    case Enum.subtraction:
+      res.status(200).json({
+        slackUsername: "mikey",
+        result: x - y,
+        operation_type,
+      });
 
-  if (operation_type.trim() === "addition") {
-    res.status(200).json({
-      slackUsername: "mikey",
-      result: x + y,
-      operation_type,
-    });
-  }
+    case Enum.addition:
+      res.status(200).json({
+        slackUsername: "mikey",
+        result: x + y,
+        operation_type: operation_type.value,
+      });
 
-  if (operation_type.trim() === "multiplication") {
-    res.status(200).json({
-      slackUsername: "mikey",
-      result: x * y,
-      operation_type,
-    });
+    case Enum.multiplication:
+      res.status(200).json({
+        slackUsername: "mikey",
+        result: x * y,
+        operation_type,
+      });
+
+    default:
+      break;
   }
 });
 
@@ -53,3 +62,27 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...🏃🏾‍♂️💨`);
 });
+
+// if (operation_type.trim() === "subtraction") {
+// res.status(200).json({
+//   slackUsername: "mikey",
+//   result: x - y,
+//   operation_type,
+// });
+// }
+
+// if (operation_type.trim() === "addition") {
+// res.status(200).json({
+//   slackUsername: "mikey",
+//   result: x + y,
+//   operation_type,
+// });
+// }
+
+// if (operation_type.trim() === "multiplication") {
+// res.status(200).json({
+//   slackUsername: "mikey",
+//   result: x * y,
+//   operation_type,
+// });
+// }
